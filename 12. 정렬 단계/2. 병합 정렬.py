27afd6199a6df61804286746d9ea5524f14a34e9
Kdelphinus <delphinus@khu.ac.kr>
2021-05-27 17:15:30 +0900

    12-2 병합정렬 복습

--- 12. 정렬 단계/2. 병합 정렬.py
+++ 12. 정렬 단계/2. 병합 정렬.py
@@ -2,14 +2,14 @@
 """O(nlogn)인 정렬 알고리즘으로 풀 수 있다 -> 병합 정렬, 힙 정렬 등"""
 
 
-# 두 리스트를 병합하며 정렬하는 함수
 def merge(list1, list2):
-    i = 0
-    j = 0
+    """두 리스트를 병합하며 정렬하는 함수"""
+    i = 0  # list1의 인덱스
+    j = 0  # list2의 인덱스
 
     merged_list = []
 
-    while i < len(list1) and j < len(list2):
+    while i < len(list1) and j < len(list2):  # 두 리스트 값 중 작은 값을 리스트에 저장
         if list1[i] < list2[j]:
             merged_list.append(list1[i])
             i += 1
@@ -17,17 +17,17 @@ def merge(list1, list2):
             merged_list.append(list2[j])
             j += 1
 
-    if i == len(list1):
+    if i == len(list1):  # list1을 다 비교했다면 남은 list2를 추가
         merged_list += list2[j:]
-    elif j == len(list2):
+    elif j == len(list2):  # list2를 다 비교했다면 남은 list1을 추가
         merged_list += list1[i:]
 
     return merged_list
 
 
-# 리스트를 합병 정렬하는 리스트
 def merged_sort(temp_list):
-    if len(temp_list) <= 1:
+    """리스트를 합병 정렬하는 리스트"""
+    if len(temp_list) <= 1:  # 더 이상 나눌 수 없을 때
         return temp_list
 
     middle = len(temp_list) // 2
