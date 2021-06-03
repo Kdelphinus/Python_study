3023afc5f35fe821121a247fe12bbf0a67eac579
Kdelphinus <delphinus@khu.ac.kr>
2021-06-03 15:59:44 +0900

    14-5, 6 복습

--- 14. 백트래킹 단계/5. N-Queen.py
+++ 14. 백트래킹 단계/5. N-Queen.py
@@ -1,37 +1,32 @@
 """9663 N-Queen"""
 """i - j + n - 1과 i + j는 사진 참고"""
 
-# 체스판 크기 n x n
-n = int(input())
+import sys
 
-# 계산에 필요한 것들
-count = 0  # 퀸을 놓을 수 있는 경우의 수
-columns = [False] * n  # 같은 열에 못들어오도록
-right_diagonal = [False] * (2 * n - 1)  # 오른쪽으로 향하는 대각선에 못들어오도록
-left_diagonal = [False] * (2 * n - 1)  # 왼쪽으로 향하는 대각선에 못들어오도록
+input = sys.stdin.readline
+
+N = int(input().rstrip())  # N x N 체스판
+
+cnt = 0  # 퀸을 놓을 수 있는 수
+height = [False] * N  # 세로
+left_diagonal = [False] * (2 * N - 1)  # 왼쪽으로 내려가는 대각선
+right_diagonal = [False] * (2 * N - 1)  # 오른쪽으로 내려가는 대각선
 
 
-# 퀸을 놓을 수 있는 경우의 수를 구하는 함수
-def solution(i):
-    global count
+def N_queen(i):
+    """퀸을 놓는 방법을 구하는 함수"""
+    global cnt
 
-    # 퀸을 끝 행까지 다 놓으면 경우의 수를 추가하고 리턴
-    if i == n:
-        count += 1
+    if i == N:  # 퀸을 다 두었을 때
+        cnt += 1
         return
 
-    # 한 칸씩 열을 이동하면서 놓을 수 있는지 확인
-    for j in range(n):
-        # 지금까지 놓여 있는 퀸들의 열, 오른쪽/왼쪽 대각선에 모두 잡히지 않는다면 거기에 퀸을 놓고
-        if not (columns[j] or right_diagonal[i - j + n - 1] or left_diagonal[i + j]):
-            # 지금 놓은 퀸의 열, 오른쪽/왼쪽 대각선에 잡히는 위치를 True로 바꾼 뒤,
-            columns[j] = right_diagonal[i - j + n - 1] = left_diagonal[i + j] = True
-            # 다음 행으로 이동
-            solution(i + 1)
-            # 다 끝마치고 전 행으로 돌아오면 다른 경우의 수를 찾도록 지금 행의 퀸을 빼버림
-            columns[j] = right_diagonal[i - j + n - 1] = left_diagonal[i + j] = False
+    for j in range(N):  # 세로, 왼쪽 대각선, 오른쪽 대각선에 퀸이 없을 경우 두고 다음 행으로 이동
+        if not (height[j] or left_diagonal[i + j] or right_diagonal[i - j + N - 1]):
+            height[j] = left_diagonal[i + j] = right_diagonal[i - j + N - 1] = True
+            N_queen(i + 1)
+            height[j] = left_diagonal[i + j] = right_diagonal[i - j + N - 1] = False
 
 
-# 0행부터 시작
-solution(0)
-print(count)
+N_queen(0)
+print(cnt)
--- 14. 백트래킹 단계/6. 스도쿠.py
+++ 14. 백트래킹 단계/6. 스도쿠.py
@@ -1,55 +1,57 @@
 """2580 스도쿠"""
 
-# 스도쿠와 빈 칸
-sudoku = [list(map(int, input().split())) for _ in range(9)]
-zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
+import sys
+
+input = sys.stdin.readline
+
+sudoku = [list(map(int, input().split())) for _ in range(9)]  # 완성되지 않은 스도쿠
+zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]  # 빈 칸의 좌표
 
 
 def is_possible(i, j):
     """빈 칸에 들어갈 수 있는 수를 구하는 함수"""
     possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    # 행열 검사
     for k in range(9):
-        if sudoku[i][k] in possible:
+        if sudoku[i][k] in possible:  # 열에 들어있는 숫자 확인
             possible.remove(sudoku[i][k])
-        if sudoku[k][j] in possible:
+        if sudoku[k][j] in possible:  # 행에 들어있는 숫자 확인
             possible.remove(sudoku[k][j])
 
-    # 박스 검사
-    i //= 3
-    j //= 3
-    for p in range(i * 3, i * 3 + 3):
-        for q in range(j * 3, j * 3 + 3):
+    i = i // 3 * 3  # 박스 가로 길이
+    j = j // 3 * 3  # 박스 세로 길이
+
+    for p in range(i, i + 3):  # 박스 안에 들어있는 숫자 확인
+        for q in range(j, j + 3):
             if sudoku[p][q] in possible:
                 possible.remove(sudoku[p][q])
 
     return possible
 
 
-answer = False  # 답 출력 여부
+flag = False  # 정답을 출력했는지 확인하는 변수
 
 
-def solution(x):
-    global answer
+def sudoku_solution(x):
+    """스도쿠 빈 칸을 채워주는 함수"""
+    global flag
 
-    if answer:  # 이미 답이 출력된 경우
+    if flag:  # 답을 이미 출력했을 때
         return
 
-    if x == len(zeros):  # 마지막 빈칸까지 다 채웠을 경우
+    if x == len(zeros):  # 모든 빈 칸을 채웠을 때
         for row in sudoku:
-            print(*row)  # 리스트 안에 수를 한 칸씩 띄어 차례대로 출력
-        answer = True
+            print(*row)
+        flag = True
         return
 
-    else:
-        (i, j) = zeros[x]
-        possible = is_possible(i, j)
+    i, j = zeros[x]  # 빈 칸의 좌표를 가져옴
+    possible = is_possible(i, j)  # 들어갈 수 있는 숫자를 계산
 
-        for num in possible:
-            sudoku[i][j] = num  # 가능한 수 중 하나를 넣음
-            solution(x + 1)  # 다음 빈칸으로 이동
-            sudoku[i][j] = 0  # 정답이 없을 경우 초기화
+    for num in possible:
+        sudoku[i][j] = num  # 들어갈 수 있는 숫자를 채움
+        sudoku_solution(x + 1)  # 다음 빈 칸으로 넘어감
+        sudoku[i][j] = 0  # 지금 숫자가 정답이 아니면 다시 빈 칸으로 비워둠
 
 
-solution(0)
+sudoku_solution(0)  # 첫 번째 빈칸부터 확인
