f54b8deb90f75a0f25b71abea49f04cf17a1cea7
Kdelphinus <delphinus@khu.ac.kr>
2021-06-03 15:24:07 +0900

    14-1, 2, 3, 4 직접 구현

--- 14. 백트래킹 단계/2. N과 M (2).py
+++ 14. 백트래킹 단계/2. N과 M (2).py
@@ -1,5 +1,6 @@
 """15650 N과 M (2)"""
 
+"""내장 함수 이용"""
 from itertools import combinations
 
 # 1부터 n까지 숫자 중 m의 원소를 이용하여 조합을 만들 예정
@@ -11,3 +12,29 @@ C = combinations(range(1, n + 1), m)
 # 출력
 for i in C:
     print(" ".join(map(str, i)))
+
+# ------------------------------------------------------------------------------
+
+"""직접 구현"""
+
+
+def DFS(l, num, count):
+    """combinations를 구현한 함수"""
+    if l == count:
+        print(*result)
+        return
+
+    for i in range(num):
+        if l == 0:  # 처음 값을 넣을 때
+            result.append(i + 1)
+            DFS(l + 1, num, count)
+            result.pop()
+        elif result[-1] < i + 1:  # 이전 값보다 큰 값만 들어가야 한다
+            result.append(i + 1)
+            DFS(l + 1, num, count)
+            result.pop()
+
+
+num, count = map(int, input().split())
+result = []
+DFS(0, num, count)
--- 14. 백트래킹 단계/4. N과 M (4).py
+++ 14. 백트래킹 단계/4. N과 M (4).py
@@ -1,5 +1,6 @@
 """15652 N과 M (4)"""
 
+""" 내장 함수 이용"""
 from itertools import combinations_with_replacement
 
 # 1부터 n까지 숫자 중 m의 원소를 이용하여 조합을 만들 예정
@@ -11,3 +12,31 @@ print(
         list(map(" ".join, combinations_with_replacement(map(str, range(1, n + 1)), m)))
     )
 )
+
+# ------------------------------------------------------------------------------
+
+"""직접 구현"""
+import sys
+
+input = sys.stdin.readline
+
+
+def DFS(l, num, count):
+    if l == count:
+        print(*result)
+        return
+
+    for i in range(num):
+        if not result:
+            result.append(i + 1)
+            DFS(l + 1, num, count)
+            result.pop()
+        elif result[-1] <= i + 1:
+            result.append(i + 1)
+            DFS(l + 1, num, count)
+            result.pop()
+
+
+num, count = map(int, input().split())
+result = []
+DFS(0, num, count)
