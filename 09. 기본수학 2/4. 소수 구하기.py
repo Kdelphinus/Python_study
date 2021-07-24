7db3aea0ba4931f18d0f84fc931639109fab6f4c
Kdelphinus <delphinus@khu.ac.kr>
2021-07-24 19:50:08 +0900

    9-4 효율성 개선

--- 09. 기본수학 2/4. 소수 구하기.py
+++ 09. 기본수학 2/4. 소수 구하기.py
@@ -1,20 +1,21 @@
 """ 1929 소수 구하기 """
 
+
+def prime_list(n):
+    """에라토스테네스의 체로 소수를 구하는 함수"""
+    prime_number = [True] * (n + 1)
+    prime_number[1] = False
+
+    for i in range(2, int(n ** 0.5) + 2):
+        if prime_number[i]:
+            for j in range(i + i, n + 1, i):
+                prime_number[j] = False
+
+    return prime_number
+
+
 m, n = map(int, input().split())
-
-
-def isPrime(num):
-    if num <= 1:
-        return False
-
-    i = 2
-    while i * i <= num:  # 제곱근까지만 계산, 2와 3은 바로 True
-        if num % i == 0:
-            return False
-        i += 1
-    return True
-
-
+prime_number = prime_list(n)
 for i in range(m, n + 1):
-    if isPrime(i):  # isPrime(i)가 True일 때만 출력
+    if prime_number[i]:
         print(i)
