fe97c25f28b087fb4874e6e4a2ad3e12be528289
Kdelphinus <delphinus@khu.ac.kr>
2021-08-18 15:50:22 +0900

    18-6 다른 방법으로 풀어봄, 시간은 비슷함

--- 18. 스택/06. 오큰수.py
+++ 18. 스택/06. 오큰수.py
@@ -2,25 +2,49 @@
 
 import sys
 
-num = int(sys.stdin.readline().rstrip())  # 주어진 수의 개수
-numbers = list(map(int, sys.stdin.readline().rstrip().split()))  # 주어진 수가 담긴 리스트
+input = sys.stdin.readline
+
+# version 1
+# num = int(input())  # 주어진 수의 개수
+# numbers = list(map(int, input().split()))  # 주어진 수가 담긴 리스트
+# stack = []
+# anw = [-1 for _ in range(num)]
+
+# stack.append(0)
+# i = 1
+
+# while i < num and stack:  # 스택에 값이 있고 인덱스가 범위 안에 있을 때만 진행
+#    while (
+#        stack and numbers[stack[-1]] < numbers[i]
+#    ):  #  스택에 값이 있고 스택의 마지막에 저장된 인덱스 값보다 비교하는 값이 더 크면
+#        anw[stack[-1]] = numbers[i]  # 오큰수를 저장
+#        stack.pop()  # 저장한 인덱스는 제거한다
+
+#    stack.append(i)  # 기준 대상을 다음 인덱스로 이동
+#    i += 1  # 비교할 대상을 다음 인덱스로 이동
+
+# print(*anw)
+
+# --------------------------------------------------------------------------------------------------------
+
+# version 2, c로 풀었던 방법
+num = int(input())
+number = list(map(int, input().split()))
 stack = []
-anw = [-1 for _ in range(num)]
+anw = []
 
-stack.append(0)
-i = 1
+for i in range(num - 1, -1, -1):
+    while stack and stack[-1] <= number[i]:
+        stack.pop()
+    if not stack:
+        anw.append(-1)
+    else:
+        anw.append(stack[-1])
 
-while i < num and stack:  # 스택에 값이 있고 인덱스가 범위 안에 있을 때만 진행
-    while (
-        stack and numbers[stack[-1]] < numbers[i]
-    ):  #  스택에 값이 있고 스택의 마지막에 저장된 인덱스 값보다 비교하는 값이 더 크면
-        anw[stack[-1]] = numbers[i]  # 오큰수를 저장
-        stack.pop()  # 저장한 인덱스는 제거한다
+    stack.append(number[i])
 
-    stack.append(i)  # 기준 대상을 다음 인덱스로 이동
-    i += 1  # 비교할 대상을 다음 인덱스로 이동
-
-print(*anw)
+for i in range(num - 1, -1, -1):
+    print(anw[i], end=" ")
 
 
 # --------------------------------------------------------------------------------------------------------
