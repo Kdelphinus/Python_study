33a0a7d8d7ca995aaeee2f9e4df6f7f6d12645b8
Kdelphinus <delphinus@khu.ac.kr>
2021-08-25 16:45:41 +0900

    좋은 풀이 추가

--- level 1/직업군 추천하기.py
+++ level 1/직업군 추천하기.py
@@ -18,6 +18,18 @@ def solution(table, languages, preference):
     return table[0][0]
 
 
+"""풀이는 같으나 훨씬 간단"""
+# def solution(table, languages, preference):
+#    score = {}
+#    for t in table:
+#        for lang, pref in zip(languages, preference):
+#            if lang in t.split():
+#                score[t.split()[0]] = (
+#                    score.get(t.split()[0], 0) + (6 - t.split().index(lang)) * pref
+#                )
+#    return sorted(score.items(), key=lambda item: [-item[1], item[0]])[0][0]
+
+
 table = [
     "SI JAVA JAVASCRIPT SQL PYTHON C#",
     "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
