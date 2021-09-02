188875369b53bf37b150226c7bfa7c31f62a167f
Kdelphinus <delphinus@khu.ac.kr>
2021-09-02 18:53:49 +0900

    해결, 파이썬은 내장함수를 많이 알수록 효율적

--- level 2/다음 큰 숫자.py
+++ level 2/다음 큰 숫자.py
@@ -1,30 +1,11 @@
 """연습문제"""
 
 
-def binary_number(n):
-    cnt = 1
-    while n >= 2 ** cnt:
-        cnt += 1
-
-    number = ["0"] * cnt
-    number[0] = "1"
-
-    while n > 2:
-        n -= 2 ** (cnt - 1)
-        cnt = 1
-        while n >= 2 ** cnt:
-            cnt += 1
-        number[len(number) - cnt] = "1"
-
-    return "".join(number)
-
-
 def solution(n):
-    cnt = str(binary_number(n)).count("1")
+    cnt = format(n, 'b').count("1")
     m = n + 1
-    bm = binary_number(m)
     while True:
-        bm = binary_number(m)
+        bm = format(m, 'b')
         if bm.count("1") == cnt:
             return m
         m += 1
