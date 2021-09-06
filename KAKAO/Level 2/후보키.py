52490720e7139dd93e1823b6a5db8ef70da29164
Kdelphinus <delphinus@khu.ac.kr>
2021-09-07 00:00:23 +0900

    해결

--- KAKAO/Level 2/후보키.py
+++ KAKAO/Level 2/후보키.py
@@ -1,10 +1,42 @@
 """2019 KAKAO BLIND RECRUITMENT"""
 
+# 해설: https://kyome.tistory.com/109
+
+from itertools import combinations
+
+
+def solution(relation):
+    total = []
+    unique_index = []
+    row_size = len(relation)
+    col_size = len(relation[0])
+
+    for i in range(1, col_size + 1):
+        total.extend([set(k) for k in combinations([j for j in range(col_size)], i)])
+
+    for comb in total:
+        valid_set = set()
+        for row in range(row_size):
+            temp = ""
+            for col in comb:
+                temp += relation[row][col]
+            valid_set.add(temp)
+
+        if len(valid_set) == row_size:
+            unique_index.append(comb)
+
+    del_set = set()
+    for standard in unique_index:
+        for idx, compare in enumerate(unique_index):
+            if standard.issubset(compare) and standard != compare:
+                del_set.add(idx)
+
+    return len(unique_index) - len(del_set)
+
 
 # -----------------------------------------------------------------------------------
 
 """첫 풀이, 85%의 정답률"""
-from itertools import combinations
 
 
 def solution(relations):
