91d463e6d69338e6aa5f7f41aee26f144f68a221
Kdelphinus <delphinus@khu.ac.kr>
2021-10-13 19:50:09 +0900

    주석 추가

--- Programmers/위클리 챌린지/Level 2/교점에 별 만들기.py
+++ Programmers/위클리 챌린지/Level 2/교점에 별 만들기.py
@@ -3,6 +3,25 @@ INF = float("inf")
 
 
 def intersection_point(line1, line2):
+    """intersection_point 두 직선의 교점을 찾는 함수
+
+    <equation>
+    Ax + By + E = 0
+    Cx + Dy + F = 0
+
+    x = (BF - ED) / (AD - BC)
+    y = (EC - AF) / (AD - BC)
+
+    if AD - BC == 0: 평행 or 일치
+
+    Args:
+        line1 (list): ax + by + c = 0에서 [a, b, c]가 저장된 리스트
+        line2 (list): ax + by + c = 0에서 [a, b, c]가 저장된 리스트
+
+    Returns:
+        [x, y] (list): 교점의 좌표
+        0 (int): 교점이 없거나 무수히 많은 경우
+    """
     if (line1[0] * line2[1] - line1[1] * line2[0]) == 0:
         return 0
 
@@ -27,44 +46,55 @@ def intersection_point(line1, line2):
 
 
 def solution(line):
-    answer = []
-    inter_points = []
-    boundary = [-INF, -INF, INF, INF]  # 위, 오른쪽, 아래, 왼쪽 / 시계 방향
+    answer = []  # 출력할 문자열들을 저장할 리스트
+    inter_points = []  # 교점을 저장할 리스트
+    boundary = [-INF, -INF, INF, INF]  # 위, 오른쪽, 아래, 왼쪽
 
+    # 교점 찾기
     for i in range(len(line) - 1):
         for j in range(i + 1, len(line)):
             inter_point = intersection_point(line[i], line[j])
+
+            # 아직 저장하지 않은 교점이 있을 때
             if inter_point != 0 and inter_point not in inter_points:
-                inter_points.append(inter_point)
+                inter_points.append(inter_point)  # 교점 추가
 
-                boundary[0] = max(boundary[0], inter_point[1])
-                boundary[1] = max(boundary[1], inter_point[0])
-                boundary[2] = min(boundary[2], inter_point[1])
-                boundary[3] = min(boundary[3], inter_point[0])
+                # 교점들의 경계 최신화
+                boundary[0] = max(boundary[0], inter_point[1])  # 위
+                boundary[1] = max(boundary[1], inter_point[0])  # 오른쪽
+                boundary[2] = min(boundary[2], inter_point[1])  # 왼쪽
+                boundary[3] = min(boundary[3], inter_point[0])  # 아래
 
+    # 교점이 한 개일 때
     if len(inter_points) == 1:
         return ["*"]
 
-    idx = 0
-    inter_points.sort(key=lambda x: (-x[1], x[0]))
-    width = boundary[1] - boundary[3] + 1
+    idx = 0  # inter_points의 인덱스
+    inter_points.sort(key=lambda x: (-x[1], x[0]))  # y좌표 내림차순, x좌표 오름차순
+    width = boundary[1] - boundary[3] + 1  # 가로 길이
     while boundary[0] >= boundary[2] and idx < len(inter_points):
+        # 현재 y좌표에 교점이 있을 때
         if inter_points[idx][1] == boundary[0]:
             tmp = ""
             for i in range(boundary[3], boundary[1] + 1):
+                # 교점이 있는 위치라면
                 if (
                     idx < len(inter_points)
                     and i == inter_points[idx][0]
                     and inter_points[idx][1] == boundary[0]
                 ):
                     tmp += "*"
-                    idx += 1
+                    idx += 1  # 다음 교점 확인
+
+                # 교점이 없는 위치라면
                 else:
                     tmp += "."
             answer.append(tmp)
+
+        # 교점이 없는 위치라면
         else:
             answer.append("." * width)
-        boundary[0] -= 1
+        boundary[0] -= 1  # y좌표를 하나 낮춘다
 
     return answer
 
