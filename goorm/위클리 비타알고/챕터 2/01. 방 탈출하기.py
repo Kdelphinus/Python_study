310c739a3e453a9e4756f4aae01c096d8fc62b4f
Kdelphinus <delphinus@khu.ac.kr>
2021-11-18 17:05:05 +0900

    bisect을 사용한 풀이 추가

--- goorm/위클리 비타알고/챕터 2/01. 방 탈출하기.py
+++ goorm/위클리 비타알고/챕터 2/01. 방 탈출하기.py
@@ -24,3 +24,21 @@ for c in input().split():
         print(1)
     else:
         print(0)
+
+# -----------------------------------------------------------------------------------
+
+"""이분탐색 라이브러리 사용"""
+from bisect import bisect_left, bisect_right
+
+
+example_len = int(input())
+example = list(map(int, input().split()))
+example.sort()  # 이분 탐색을 위해 정렬
+check_num = int(input())
+for c in input().split():
+    # bisect_left : 처음 나오는 인덱스, bisect_right : 마지막 숫자 다음 인덱스
+    if bisect_right(example, int(c)) - bisect_left(example, int(c)) > 0:
+        print(1)
+    else:
+        print(0)
+
