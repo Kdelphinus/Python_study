ecf322361b8af832ee80f8267eb612a9931fb172
Kdelphinus <delphinus@khu.ac.kr>
2021-11-18 16:25:24 +0900

    해결

--- goorm/위클리 비타알고/챕터 1/11. 1차원 뿌요뿌요.py
+++ goorm/위클리 비타알고/챕터 1/11. 1차원 뿌요뿌요.py
@@ -1,43 +1,37 @@
-# 다섯문제 불통과
+# stack 활용
 num, condition = map(int, input().split())
-string = input() + '.'
-stack = [string[0]]
-
-cnt = 1
-idx = 1
+string = input()
+stack = []
+idx = 0
 while idx < len(string):
-	while stack[-1] == string[idx]:
-		idx += 1
-		cnt += 1
-	
-	if cnt >= condition:
-		stack.pop()
-	else:
-		tmp = stack[-1]
-		for _ in range(1, cnt):
-			stack.append(tmp)
-	
-	if stack and stack[-1] == string[idx]:
-		cnt = 2
-	else:
-		stack.append(string[idx])
-		cnt = 1
-	idx += 1
+    # 스택이 비어있거나 앞 문자와 같지 않으면 스택에 삽입
+    if not stack or stack[-1][0] != string[idx]:
+        stack.append([string[idx], 1])  # [문자, 반복된 횟수]
+        idx += 1
 
-stack.pop()
-if not stack:
-	print("CLEAR!")
-else:
-	for s in stack:
-		print(s, end="")
+    # 앞 문자와 다를 때까지 한 칸씩 탐색하며 문자가 반복된 횟수를 늘린다
+    while idx < len(string) and stack[-1][0] == string[idx]:
+        idx += 1
+        stack[-1][1] += 1
+
+    # 반복된 횟수가 조건에 부합하면 빼준다
+    if stack and stack[-1][1] >= condition:
+        stack.pop()
+
+if not stack:  # 스택이 비었다면 클리어
+    print("CLEAR!")
+else:  # 남아있다면 남은 것을 출력
+    for s, cnt in stack:
+        for c in range(cnt):
+            print(s, end="")
 
 # -----------------------------------------------------------------------
 
 # 시간 초과
-#num, condition = map(int, input().split())
-#string = input()
+# num, condition = map(int, input().split())
+# string = input()
 
-#while True:
+# while True:
 #    cnt = 1
 #    idx = 1
 #    char = string[0]
@@ -46,10 +40,10 @@ else:
 #        while idx < len(string) and char == string[idx]:
 #            cnt += 1
 #            idx += 1
-        
+
 #        if cnt >= condition:
 #            change.append(char * cnt)
-        
+
 #        if idx < len(string):
 #            char = string[idx]
 #            idx += 1
