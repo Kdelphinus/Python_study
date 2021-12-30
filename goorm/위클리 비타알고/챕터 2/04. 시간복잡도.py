d68521d1ae577070fe57f03f7a24e8cf45d3f35d
Myoung Jun Ko <68101516+Kdelphinus@users.noreply.github.com>
2021-12-30 23:58:41 +0900

    Delete goorm directory

--- goorm/위클리 비타알고/챕터 2/04. 시간복잡도.py
+++
@@ -1,9 +0,0 @@
-"""백준 17-11 문제와 동일"""
-num = int(input())
-# 2 * 5의 개수에 따라 뒤에 연속되는 0의 개수 결정
-# 2는 너무 많기에 5의 제곱승들에 대해만 계산하면 됨
-divide = [5 ** i for i in range(1, 15)]
-anw = 0
-for i in divide:
-    anw += num // i
-print(anw)
