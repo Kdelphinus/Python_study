e6c8ac53cccec8e72d03f979b33216a53bf942c0
Kdelphinus <delphinus@khu.ac.kr>
2022-06-18 20:53:38 +0900

    solve

---
+++ Baekjoon/bronze/bronze II/1977_perfect_square_number.py
@@ -0,0 +1,12 @@
+m = int(input())
+n = int(input())
+start = int(m ** 0.5) if m == int(m ** 0.5) ** 2 else int(m ** 0.5) + 1
+psn = []
+while start <= n / start:
+    psn.append(start ** 2)
+    start += 1
+if len(psn) == 0:
+    print(-1)
+else:
+    print(sum(psn))
+    print(psn[0])
