57e236a2a0e503a3490c06a0132ffab4f0ca1532
Kdelphinus <delphinus@khu.ac.kr>
2022-07-04 17:10:43 +0900

    solve

--- Baekjoon/solve_step_by_step/10. 브루트 포스/1. 블랙잭.py
+++ Baekjoon/solve_step_by_step/10. 브루트 포스/1. 블랙잭.py
@@ -1,5 +1,39 @@
 """2798 블랙잭"""
 
+# # num: 카드의 개수, max: 목표 합
+# num, limit = map(int, input().split())
+#
+# # 내가 가진 카드
+# card = list(map(int, input().split()))
+#
+# # 합의 초기값
+# sum = 0
+#
+# for i in range(len(card) - 2):
+#     for j in range(i + 1, len(card) - 1):
+#         for k in range(j + 1, len(card)):
+#             if i < j and j < k:
+#                 if card[i] + card[j] + card[k] <= limit:
+#                     sum = max(card[i] + card[j] + card[k], sum)
+# print(sum)
+
+##############################################################################
+
+"""2022/07/04 때 간단하게 수정"""
+
+
+def blackjack(card: list, num: int, limit: int):
+    sum = 0
+    for i in range(num - 2):
+        for j in range(i + 1, num - 1):
+            for k in range(j + 1, num):
+                if card[i] + card[j] + card[k] < limit:
+                    sum = max(card[i] + card[j] + card[k], sum)
+                elif card[i] + card[j] + card[k] == limit:
+                    return limit
+    return sum
+
+
 # num: 카드의 개수, max: 목표 합
 num, limit = map(int, input().split())
 
@@ -8,11 +42,4 @@ card = list(map(int, input().split()))
 
 # 합의 초기값
 sum = 0
-
-for i in range(len(card) - 2):
-    for j in range(i + 1, len(card) - 1):
-        for k in range(j + 1, len(card)):
-            if i < j and j < k:
-                if card[i] + card[j] + card[k] <= limit:
-                    sum = max(card[i] + card[j] + card[k], sum)
-print(sum)
+print(blackjack(card, num, limit))
