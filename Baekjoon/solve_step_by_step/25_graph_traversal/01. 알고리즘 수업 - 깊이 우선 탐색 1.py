d527c5873080731fd55804a31351f535c53fce56
Kdelphinus <delphinus@khu.ac.kr>
2022-07-07 13:45:21 +0900

    solve

---
+++ Baekjoon/solve_step_by_step/25_graph_traversal/01. 알고리즘 수업 - 깊이 우선 탐색 1.py
@@ -0,0 +1,34 @@
+"""24479 알고리즘 수업 - 깊이 우선 탐색 1 / silver II"""
+
+import sys
+
+sys.setrecursionlimit(10 ** 8)
+input = sys.stdin.readline
+cnt = 1
+
+
+def dfs(r: int):
+    global cnt
+
+    visited[r] = cnt
+    cnt += 1
+    for l in linked[r]:
+        if not visited[l]:
+            dfs(l)
+
+
+n, m, r = map(int, input().split())
+linked = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for _ in range(m):
+    n1, n2 = map(int, input().split())
+    linked[n1].append(n2)
+    linked[n2].append(n1)
+
+for i in range(1, n + 1):
+    linked[i].sort()
+
+dfs(r)
+for i in visited[1:]:
+    print(i)
