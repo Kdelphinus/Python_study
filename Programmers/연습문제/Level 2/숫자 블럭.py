3b4ef7745cb6496b79828cc5a5d8f80a4a540258
Kdelphinus <delphinus@khu.ac.kr>
2022-08-05 18:35:22 +0900

    solve, but estimate test case error

--- Programmers/연습문제/Level 2/숫자 블럭.py
+++ Programmers/연습문제/Level 2/숫자 블럭.py
@@ -4,10 +4,18 @@
 def max_divisor(num: int):
     if num == 1:
         return 0
-    for i in range(int(num ** 0.5), 1, -1):
-        if num % i == 0 and num // i < 1000000000:
-            return num // i
-    return 1
+    max_div = 1
+    for i in range(2, int(num ** 0.5) + 1):
+        if i > 10000000:
+            break
+        if num % i == 0:
+            if num // i <= 10000000:
+                return num // i
+            # 약수이기 때문에 이 조건이 추가되어야 맞는 것처럼 보이나 테스트 케이스에 문제가 있는지 이를 주석처리해야 통과
+            # 관련 링크: https://school.programmers.co.kr/questions/15757
+            # elif i <= 10000000:
+            #     max_div = max(max_div, i)
+    return max_div
 
 
 def number_block(begin: int, end: int):
