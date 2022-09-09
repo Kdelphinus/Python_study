9f05e7fa742c58a900585014edc7b80318730015
Kdelphinus <delphinus@khu.ac.kr>
2022-09-09 17:19:39 +0900

    solve

---
+++ Baekjoon/bronze/bronze II/15829_Hashing.py
@@ -0,0 +1,14 @@
+from string import ascii_lowercase
+
+r = 31
+mod = 1234567891
+alphabet = {}
+for i, s in enumerate(ascii_lowercase):
+    alphabet[s] = i + 1
+
+num = int(input())
+str = input()
+ans = 0
+for i, s in enumerate(str):
+    ans += alphabet[s] * (r ** i)
+print(ans % mod)
