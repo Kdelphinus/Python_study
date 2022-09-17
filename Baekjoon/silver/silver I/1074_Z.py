8f169f6bb74bc45e4bd8402c5d4991cad68b70a2
Kdelphinus <delphinus@khu.ac.kr>
2022-09-17 17:34:02 +0900

    solve

--- Baekjoon/silver/silver I/1074_Z.py
+++ Baekjoon/silver/silver I/1074_Z.py
@@ -3,28 +3,28 @@ import sys
 sys.setrecursionlimit(10 ** 8)
 
 
-def find_position(N: int, r: int, c: int, start: int):
-    if N == 1:
+def find_position(n: int, r: int, c: int, start: int):
+    if n == 1:
         if r % 2 == 0 and c % 2 == 0:
-            print(start)
+            return start
         elif r % 2 == 0:
-            print(start + 1)
+            return start + 1
         elif c % 2 == 0:
-            print(start + 2)
+            return start + 2
         else:
-            print(start + 3)
+            return start + 3
 
-    side = (2 ** N) / 2
+    side = 2 ** n // 2
     if side > r and side > c:
-        find_position(N - 1, r % side, c % side, start)
+        return find_position(n - 1, r % side, c % side, start)
     elif side > r:
-        find_position(N - 1, r % side, c % side, start + side ** 2)
+        return find_position(n - 1, r % side, c % side, start + side ** 2)
     elif side > c:
-        find_position(N - 1, r % side, c % side, start + (side ** 2) * 2)
+        return find_position(n - 1, r % side, c % side, start + side ** 2 * 2)
     else:
-        find_position(N - 1, r % side, c % side, start + (side ** 2) * 3)
+        return find_position(n - 1, r % side, c % side, start + side ** 2 * 3)
 
 
 if __name__ == "__main__":
-    N, r, c = map(int, input().split())
-    find_position(N, r, c, 0)
+    n, r, c = map(int, input().split())
+    print(find_position(n, r, c, 0))
