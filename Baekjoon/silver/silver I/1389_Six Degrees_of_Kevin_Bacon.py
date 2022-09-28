49d77a99fba3a45afff9203d294868902027eb8d
Kdelphinus <kmj951015@gmail.com>
2022-09-28 20:10:42 +0900

    solve

---
+++ Baekjoon/silver/silver I/1389_Six Degrees_of_Kevin_Bacon.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+
+def kevin_bacon(me: int, n: int, linked: list):
+    total = 0
+    queue = deque()
+    queue.append((me, 0))
+    visited = [0] * n
+    visited[me] = 1
+
+    while queue:
+        user, dist = queue.popleft()
+        if not visited[user]:
+            visited[user] = dist
+
+        for i in range(n):
+            if not visited[i] and i in linked[user]:
+                queue.append((i, dist + 1))
+
+    return sum(visited) - 1
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    linked = [[] for _ in range(n)]
+    for _ in range(m):
+        u1, u2 = map(int, input().split())
+        linked[u1 - 1].append(u2 - 1)
+        linked[u2 - 1].append(u1 - 1)
+
+    ans, min_kevin = 0, float("inf")
+    for i in range(n):
+        tmp = kevin_bacon(i, n, linked)
+        if min_kevin > tmp:
+            ans, min_kevin = i, tmp
+    print(ans + 1)
