dd01dd3d2c1f3535ee999075a055405ef8a63a90
Kdelphinus <kmj951015@gmail.com>
2022-10-18 05:42:01 +0900

    time reduction

--- Baekjoon/gold/gold III/16236_baby_shark.py
+++ Baekjoon/gold/gold III/16236_baby_shark.py
@@ -1,63 +1,70 @@
+# minkj1992의 풀이: https://www.acmicpc.net/source/14400838
+# 위의 풀이로 시간 단축
+
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
 
-def what_time(i: int, j: int, n: int, baby: int, queue: deque):
+def update(y: int, x: int):
+    global shark
+
+    _, _, size, level = shark
+    board[y][x] = 0
+    shark = [y, x, size + 1, size + 1] if level == 1 else [y, x, size, level - 1]
+
+
+def baby_shark(n: int):
+    global shark
+
+    cnt = 0
+    sy, sx, size, _ = shark
+    queue = [(sy, sx)]
     directions = (1, 0), (-1, 0), (0, 1), (0, -1)
     visit = [[0] * n for _ in range(n)]
-    visit[queue[0][0]][queue[0][1]] = 1
+    visit[sy][sx] = 1
     while queue:
-        y, x, cnt = queue.popleft()
-
-        for dy, dx in directions:
-            ny, nx = y + dy, x + dx
-            if (
-                0 <= ny < n
-                and 0 <= nx < n
-                and not visit[ny][nx]
-                and board[ny][nx] <= baby
-            ):
-                if ny == i and nx == j:
-                    return cnt + 1
-                visit[ny][nx] = 1
-                queue.append([ny, nx, cnt + 1])
-    return -1
-
-
-def baby_shark(n: int, baby: int, shark: list):
-    survive, eat = 0, 0
-    while fishes:
-        during = [float("inf")] * 4
-        for idx, (i, j, t) in enumerate(fishes):
-            if t < baby:
-                tmp = what_time(i, j, n, baby, deque([shark]))
-                if 0 < tmp < during[0]:
-                    during = [tmp, idx, i, j]
-        if during[0] == float("inf"):
-            return survive
-        del fishes[during[1]]
-        eat += 1
-        if eat == baby:
-            baby += 1
-            eat = 0
-        survive += during[0]
-        board[shark[0]][shark[1]] = 0
-        shark = during[2:] + [0]
-    return survive
+        cnt += 1
+        can_eat = []
+        len_q = len(queue)
+        for _ in range(len_q):
+            y, x = queue.pop(0)
+            for dy, dx in directions:
+                ny, nx = y + dy, x + dx
+                if (
+                    0 <= ny < n
+                    and 0 <= nx < n
+                    and not visit[ny][nx]
+                    and board[ny][nx] <= size
+                ):
+                    visit[ny][nx] = 1
+                    if 0 < board[ny][nx] < size:
+                        can_eat.append((ny, nx))
+                    else:
+                        queue.append((ny, nx))
+        if can_eat:
+            fish = sorted(can_eat)[0]
+            update(*fish)
+            return cnt
+    return 0
 
 
 if __name__ == "__main__":
     n = int(input())
-    fishes, board = [], []
+    fishes, board, flag = [], [], False
     for i in range(n):
         tmp = list(map(int, input().split()))
         for j, t in enumerate(tmp):
             if 0 < t < 9:
-                fishes.append((i, j, t))
+                flag = t < 2
             elif t == 9:
-                shark = [i, j, 0]
+                shark = [i, j, 2, 2]
                 tmp[j] = 0
         board.append(tmp)
-    print(baby_shark(n, 2, shark))
+    total = 0
+    while True:
+        tmp = baby_shark(n)
+        if not tmp:
+            break
+        total += tmp
+    print(total)
