f5699609ffa70e600d35d2765d2b2dee979a7b1f
Kdelphinus <delphinus@khu.ac.kr>
2022-10-21 20:53:45 +0900

    remove ignore files

--- goorm/위클리 비타알고/챕터 3/01. T세포.py
+++
@@ -1,8 +0,0 @@
-size = int(input())
-size_binary = format(size, "b")  # 이진수로 표현하여 문자열로 반환
-
-print(size_binary.count("1"))  # 1이 있는 곳이 분열을 해서 몸집을 키울 때
-for i in range(len(size_binary) - 1, -1, -1):
-    if size_binary[i] == "1":
-        print(len(size_binary) - i - 1, end=" ")
-
--- study/codeit/2. (미완)python_intermediate/cil/__init__.py
+++
@@ -1,3 +0,0 @@
-from cil.utils.io import *
-from cil.utils.visualize import *
-from cil.processing import *
