54bd4a9529a03be9e2f28ebd3c29462eeb8f2487
Kdelphinus <delphinus@khu.ac.kr>
2022-11-04 10:51:58 +0900

    rename solve_step_by_step

---
+++ Baekjoon/solve_step_by_step/09_2d_list/2. 최댓값.py
@@ -0,0 +1,11 @@
+"""2566 최댓값"""
+
+max_v = -1
+position = [0, 0]
+for i in range(9):
+    tmp = list(map(int, input().split()))
+    if max_v < max(tmp):
+        max_v = max(tmp)
+        position = [i + 1, tmp.index(max_v) + 1]
+print(max_v)
+print(*position)
