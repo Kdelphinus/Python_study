98c285c59671c33a5ccd01412268bcfb8abbb689
Kdelphinus <kmj951015@gmail.com>
2022-11-15 20:21:45 +0900

    solve

---
+++ Baekjoon/bronze/bronze IV/4714_Lunacy.py
@@ -0,0 +1,7 @@
+while True:
+    weight = float(input())
+    if weight == -1:
+        break
+    print(
+        f"Objects weighing {weight:.2f} on Earth will weigh {weight * 0.167:.2f} on the moon."
+    )
