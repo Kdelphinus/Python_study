a9514c65a8a496df965d38627f0b8e020aad6703
Kdelphinus <delphinus@khu.ac.kr>
2022-11-21 00:01:45 +0900

    unsolve

---
+++ Baekjoon/silver/silver I/25918_Polar_bear_tear_the_brackets.py
@@ -0,0 +1,33 @@
+import sys
+
+INPUT = sys.stdin.readline
+
+
+def polar_bear(s: int) -> int:
+    idx, day, tmp, stack = 0, 0, 0, []
+    while idx < len(s):
+        while idx < len(s) and s[idx] == "(":
+            if stack and stack[-1] == ")":
+                stack.pop()
+                tmp += 1
+            else:
+                stack.append(s[idx])
+                tmp = 0
+            idx += 1
+        day = max(day, tmp)
+        while idx < len(s) and s[idx] == ")":
+            if stack and stack[-1] == "(":
+                stack.pop()
+                tmp += 1
+            else:
+                stack.append(s[idx])
+                tmp = 0
+            idx += 1
+        day = max(day, tmp)
+    return -1 if stack else day
+
+
+if __name__ == "__main__":
+    _ = int(INPUT())
+    string = INPUT().rstrip()
+    print(polar_bear(string))
