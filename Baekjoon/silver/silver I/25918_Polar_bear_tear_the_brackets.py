77e0e449fe23c5778bb032b6674da5ded5cfa3e2
Kdelphinus <delphinus@khu.ac.kr>
2022-11-21 17:13:11 +0900

    i'm trying, bur unsolve

--- Baekjoon/silver/silver I/25918_Polar_bear_tear_the_brackets.py
+++ Baekjoon/silver/silver I/25918_Polar_bear_tear_the_brackets.py
@@ -3,7 +3,7 @@ import sys
 INPUT = sys.stdin.readline
 
 
-def polar_bear(s: int) -> int:
+def polar_bear(s: str) -> int:
     idx, day, tmp, stack = 0, 0, 0, []
     while idx < len(s):
         while idx < len(s) and s[idx] == "(":
@@ -12,18 +12,19 @@ def polar_bear(s: int) -> int:
                 tmp += 1
             else:
                 stack.append(s[idx])
+                day = max(day, tmp)
                 tmp = 0
             idx += 1
-        day = max(day, tmp)
         while idx < len(s) and s[idx] == ")":
             if stack and stack[-1] == "(":
                 stack.pop()
                 tmp += 1
             else:
                 stack.append(s[idx])
+                day = max(day, tmp)
                 tmp = 0
             idx += 1
-        day = max(day, tmp)
+    day = max(day, tmp)
     return -1 if stack else day
 
 
