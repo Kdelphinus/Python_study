922068cab8fb9bac2b89e49ccd95aba185f549bd
Kdelphinus <delphinus@khu.ac.kr>
2022-11-30 19:26:43 +0900

    solve

--- Baekjoon/silver/silver I/25918_Polar_bear_tear_the_brackets.py
+++ Baekjoon/silver/silver I/25918_Polar_bear_tear_the_brackets.py
@@ -1,32 +1,18 @@
+# joon6924(wonjilee)의 풀이 참고
 import sys
 
 INPUT = sys.stdin.readline
 
 
 def polar_bear(s: str) -> int:
-    idx, day, tmp, stack = 0, 0, 0, []
-    while idx < len(s):
-        while idx < len(s) and s[idx] == "(":
-            if stack and stack[-1] == ")":
-                stack.pop()
-                tmp += 1
-            else:
-                stack.append(s[idx])
-                day = max(day, tmp)
-                tmp = 0
-            idx += 1
-        day = max(day, tmp)
-        while idx < len(s) and s[idx] == ")":
-            if stack and stack[-1] == "(":
-                stack.pop()
-                tmp += 1
-            else:
-                stack.append(s[idx])
-                day = max(day, tmp)
-                tmp = 0
-            idx += 1
-    day = max(day, tmp)
-    return -1 if stack else day
+    cnt, day = 0, 0
+    for a in s:
+        if a == "(":
+            cnt += 1
+        elif a == ")":
+            cnt -= 1
+        day = max(day, abs(cnt))
+    return -1 if cnt else day
 
 
 if __name__ == "__main__":
