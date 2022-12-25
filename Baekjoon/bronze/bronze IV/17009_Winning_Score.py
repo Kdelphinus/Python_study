3bed935fb9acef312c380d041c3aad7f9365ce2a
Kdelphinus <delphinus@khu.ac.kr>
2022-12-26 00:39:57 +0900

    solve

---
+++ Baekjoon/bronze/bronze IV/17009_Winning_Score.py
@@ -0,0 +1,14 @@
+a, b = 0, 0
+a += int(input()) * 3
+a += int(input()) * 2
+a += int(input())
+b += int(input()) * 3
+b += int(input()) * 2
+b += int(input())
+
+if a > b:
+    print("A")
+elif a < b:
+    print("B")
+else:
+    print("T")
