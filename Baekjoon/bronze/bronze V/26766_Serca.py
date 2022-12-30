6295488d6dc0233ba815611251c66b3f1e55212f
Kdelphinus <delphinus@khu.ac.kr>
2022-12-30 13:09:07 +0900

    solve

---
+++ Baekjoon/bronze/bronze V/26766_Serca.py
@@ -0,0 +1,15 @@
+def print_logo():
+    print(" @@@   @@@  ")
+    print("@   @ @   @ ")
+    print("@    @    @ ")
+    print("@         @ ")
+    print(" @       @  ")
+    print("  @     @   ")
+    print("   @   @    ")
+    print("    @ @     ")
+    print("     @     ")
+
+
+n = int(input())
+for i in range(n):
+    print_logo()
