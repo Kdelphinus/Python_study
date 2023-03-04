a475bbfffc68a4fdb89dc23ce2c3b7563f5e182b
Kdelphinus <kmj951015@gmail.com>
2023-03-04 17:28:33 +0900

    rename

--- Baekjoon/silver/silver III/9095_1,2,3_plus.py
+++ Baekjoon/silver/silver III/9095_1,2,3_plus.py
@@ -6,19 +6,20 @@ A(n) = A(n - 1) + A(n - 2) + A(n - 3)
 - A(n-3), 즉 n-3을 만드는 모든 경우 + 3
 """
 
+
 def dp_init():
-	dp = [0, 1, 2, 4]
-	for _ in range(7):
-		dp.append(dp[-1] + dp[-2] + dp[-3])
-	return dp
+    dp = [0, 1, 2, 4]
+    for _ in range(7):
+        dp.append(dp[-1] + dp[-2] + dp[-3])
+    return dp
 
 
 def main():
-	dp = dp_init()
-	t = int(input())
-	for _ in range(t):
-		print(dp[int(input())])
+    dp = dp_init()
+    t = int(input())
+    for _ in range(t):
+        print(dp[int(input())])
 
 
 if __name__ == "__main__":
-	main()
+    main()
