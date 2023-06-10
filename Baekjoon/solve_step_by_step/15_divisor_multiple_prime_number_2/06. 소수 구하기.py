68eefe9195216d671df2cbbe1cb01f842ad870ac
Kdelphinus <kmj951015@gmail.com>
2023-06-10 21:31:49 +0900

    [solve_step_by_step] 15번 폴더 문제 추가

---
+++ Baekjoon/solve_step_by_step/15_divisor_multiple_prime_number_2/06. 소수 구하기.py
@@ -0,0 +1,21 @@
+""" 1929 소수 구하기 """
+
+
+def prime_list(n):
+    """에라토스테네스의 체로 소수를 구하는 함수"""
+    prime_number = [True] * (n + 1)
+    prime_number[1] = False
+
+    for i in range(2, int(n**0.5) + 2):
+        if prime_number[i]:
+            for j in range(i + i, n + 1, i):
+                prime_number[j] = False
+
+    return prime_number
+
+
+m, n = map(int, input().split())
+prime_number = prime_list(n)
+for i in range(m, n + 1):
+    if prime_number[i]:
+        print(i)
