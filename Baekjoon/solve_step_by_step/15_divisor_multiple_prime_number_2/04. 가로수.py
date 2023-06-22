9de284c780d1b0c107eed8c680a946ebe5b8485c
Kdelphinus <kmj951015@gmail.com>
2023-06-22 13:40:20 +0900

    solve

---
+++ Baekjoon/solve_step_by_step/15_divisor_multiple_prime_number_2/04. 가로수.py
@@ -0,0 +1,18 @@
+"""2485 가로수"""
+
+
+def gcd(a: int, b: int) -> int:
+    return gcd(b, a % b) if b else a
+
+
+def planting_tree(n: int, street_tree: list) -> int:
+    gcd_t = street_tree[1] - street_tree[0]
+    for i in range(2, n):
+        gcd_t = gcd(gcd_t, street_tree[i] - street_tree[i - 1])
+    return (street_tree[-1] - street_tree[0]) // gcd_t - n + 1
+
+
+if __name__ == "__main__":
+    N = int(input())
+    STREET_TREE = sorted([int(input()) for _ in range(N)])
+    print(planting_tree(N, STREET_TREE))
