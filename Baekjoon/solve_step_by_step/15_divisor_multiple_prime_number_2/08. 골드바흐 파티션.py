6f121d523d755eb859fa86cf2dd430e2c6c0f1b2
Kdelphinus <kmj951015@gmail.com>
2023-07-03 02:04:52 +0900

    solve

---
+++ Baekjoon/solve_step_by_step/15_divisor_multiple_prime_number_2/08. 골드바흐 파티션.py
@@ -0,0 +1,26 @@
+BOUNDARY = 1000000
+NUMBERS = [True] * (BOUNDARY + 1)
+
+
+def prime_number_list() -> None:
+    NUMBERS[0], NUMBERS[1] = False, False
+    for i in range(2, BOUNDARY + 1):
+        if NUMBERS[i]:
+            for j in range(i + i, BOUNDARY + 1, i):
+                if j <= BOUNDARY:
+                    NUMBERS[j] = False
+
+
+def goldbach_partition(n: int) -> int:
+    cnt = 0
+    for i in range(2, n // 2 + 1):
+        if NUMBERS[i] and NUMBERS[n - i]:
+            cnt += 1
+    return cnt
+
+
+if __name__ == "__main__":
+    T = int(input())
+    prime_number_list()
+    for _ in range(T):
+        print(goldbach_partition(int(input())))
