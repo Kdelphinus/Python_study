4b6d4817ace8ad16c9dff021774ded6966e076fb
Myoung Jun Ko <kmj951015@gmail.com>
2023-09-02 18:53:09 +0900

    :construction: brush up
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

---
+++ Baekjoon/solve_step_by_step/30_stack_2/03. 오등큰수.py
@@ -0,0 +1,21 @@
+# 풀이: https://velog.io/@wlrhkd49/%EB%B0%B1%EC%A4%80-17299-%EC%98%A4%EB%93%B1%ED%81%B0%EC%88%98-Python
+import sys
+from collections import Counter
+
+INPUT = sys.stdin.readline
+
+
+def ngf(n: int, arr: list) -> list:
+    cnt, ans = Counter(arr), [-1 for _ in range(n)]
+    stack = []
+    for i in range(n):
+        while stack and cnt[arr[stack[-1]]] < cnt[arr[i]]:
+            ans[stack.pop()] = arr[i]
+        stack.append(i)
+    return ans
+
+
+if __name__ == "__main__":
+    N = int(INPUT())
+    ARR = list(map(int, INPUT().split()))
+    print(*ngf(N, ARR))
